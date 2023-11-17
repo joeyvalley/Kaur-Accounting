e218dd7c2a652a4f72d067002f0e43cd3c64fd9f
Joseph Valle <josephrvalle@gmail.com>
2023-11-17 17:46:20 -0500

    initial commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+function Content() {
+  return (
+    <div className="container">
+      <div className="content-container">
+        <div className="header">
+        </div>
+        <div className="menu">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/Video.jsx
@@ -0,0 +1,10 @@
+function Video() {
+  return (
+    <div className="video-container">
+      <video className="video-background" autoPlay muted playsInline loop preload="auto" src="https://res.cloudinary.com/dnld1cqvy/video/upload/v1700258212/Kaur%20Accounting/video-loop-2_huxidw.mp4" type="video/mp4">
+      </video>
+    </div>
+  );
+}
+
+export default Video;
