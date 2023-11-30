01b5f40da5b31a69cd67986d54b19d52a22c1178
Joseph Valle <josephrvalle@gmail.com>
2023-11-29 19:23:37 -0500

    first round of edits from Sandeep

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,13 @@
 export default function About() {
   return (
-    <div>
-      <p>Kaur Accounting supplies responsive accounting and consulting services to individuals, entrepreneurial businesses, creatives, not-for-profit organizations and more.</p>
-      <p>Our goal is to free visionaries to do what they do best by providing clarity in all of their financial matters. And we do it with an appreciation for the entrepreneurial spirit that our clients possess.</p>
+    <div className="content-border">
+      <div className="content-border2">
+        <div className="content">
+          <span className="service-name">About Us</span>
+          <p>Kaur Accounting supplies responsive accounting and consulting services to individuals, entrepreneurial businesses, creatives, not-for-profit organizations and more.</p>
+          <p>Our goal is to free visionaries to do what they do best by providing clarity in all of their financial matters. And we do it with an appreciation for the entrepreneurial spirit that our clients possess.</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function Video() {
+  const [videoLink, setVideoLink] = useState("https://res.cloudinary.com/dnld1cqvy/video/upload/v1700503862/Kaur%20Accounting/video-loop-1_ibswlq.mp4")
+  const [isLooping, setIsLooping] = useState(false);
+
+  function handleVideoLoop() {
+    if (!isLooping) {
+      setVideoLink("https://res.cloudinary.com/dnld1cqvy/video/upload/v1700258212/Kaur%20Accounting/video-loop-2_huxidw.mp4");
+      setIsLooping(true)
+    }
+  }
+
   return (
     <>
       <div className="video-container">
         <img className="overlay" src="https://res.cloudinary.com/dnld1cqvy/image/upload/v1700514321/Kaur%20Accounting/scanlines_gpbjnj.png" alt="CRT Overlay" />
-        <video className="video-background" autoPlay muted playsInline loop preload="auto" src="https://res.cloudinary.com/dnld1cqvy/video/upload/v1700503862/Kaur%20Accounting/video-loop-1_ibswlq.mp4" type="video/mp4">
+        <video className="video-background" autoPlay muted playsInline loop={isLooping} preload="auto" src={videoLink} type="video/mp4" onEnded={handleVideoLoop}>
         </video>
       </div>
     </>
