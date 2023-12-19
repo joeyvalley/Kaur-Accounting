9a53dccffa4659dd3eede8efc177e32bc88e2ee7
Joseph Valle <josephrvalle@gmail.com>
2023-12-19 12:47:44 -0500

    fixed content links

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
 function Video() {
-  const [videoLink, setVideoLink] = useState("https://res.cloudinary.com/dnld1cqvy/video/upload/v1700503862/Kaur%20Accounting/video-loop-1_ibswlq.mp4")
+  const [videoLink, setVideoLink] = useState("https://res.cloudinary.com/dd5bgygfp/video/upload/v1703007891/kaur/video-loop-1_yo6vbs.mp4")
   const [isLooping, setIsLooping] = useState(false);
 
   function handleVideoLoop() {
     if (!isLooping) {
-      setVideoLink("https://res.cloudinary.com/dnld1cqvy/video/upload/v1700258212/Kaur%20Accounting/video-loop-2_huxidw.mp4");
+      setVideoLink("https://res.cloudinary.com/dd5bgygfp/video/upload/v1703007894/kaur/video-loop-2_eao3nd.mp4");
       setIsLooping(true)
     }
   }
@@ -14,7 +14,7 @@ function Video() {
   return (
     <>
       <div className="video-container">
-        <img className="overlay" src="https://res.cloudinary.com/dnld1cqvy/image/upload/v1700514321/Kaur%20Accounting/scanlines_gpbjnj.png" alt="CRT Overlay" />
+        <img className="overlay" src="https://res.cloudinary.com/dd5bgygfp/image/upload/v1703007780/kaur/scanlines_copy_omuxa0.png" alt="CRT Overlay" />
         <video className="video-background" autoPlay muted playsInline loop={isLooping} preload="auto" src={videoLink} type="video/mp4" onEnded={handleVideoLoop}>
         </video>
       </div>
